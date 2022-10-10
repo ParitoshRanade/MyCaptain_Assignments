8eaa0b9bd1050527c4e3e2b1a2a8873b78d682b7
ParitoshRanade <89463613+ParitoshRanade@users.noreply.github.com>
2022-10-10 08:58:41 +0530

    Delete task2.py

--- task2.py
+++
@@ -1,7 +0,0 @@
-"""
-The input of the program should be a filename along with its extension and the output will be the extension.
-"""
-
-file_name = input("Input the Filename : ")
-ext = file_name.split(".")
-print("The extension of the file is ", ext[1])
