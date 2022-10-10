53429cd655113ff17694a08f59a4c069656fea90
ParitoshRanade <89463613+ParitoshRanade@users.noreply.github.com>
2022-10-10 08:58:30 +0530

    Delete task1.py

--- task1.py
+++
@@ -1,7 +0,0 @@
-"""
-The task is to input a radius and the output should be the area of the circle
-"""
-
-radius = float(input(print("Input the radius of the circle : ")))
-area = (22*radius*radius)/7
-print("The area of the circle with radius, ", radius, "is ", area)
