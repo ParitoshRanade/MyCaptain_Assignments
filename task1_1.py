2ed587a9728949bbacd4bceecfffd158e6461921
ParitoshRanade <89463613+ParitoshRanade@users.noreply.github.com>
2022-10-10 08:59:39 +0530

    Add files via upload

---
+++ task1_1.py
@@ -0,0 +1,7 @@
+"""
+The task is to input a radius and the output should be the area of the circle
+"""
+
+radius = float(input(print("Input the radius of the circle : ")))
+area = (22*radius*radius)/7
+print("The area of the circle with radius, ", radius, "is ", area)
